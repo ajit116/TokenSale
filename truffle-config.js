b305cd27888bd03f9bea9eca1ceff181a9f7b72a
Ajit Kumar <32820874+ajit116@users.noreply.github.com>
2020-10-18 19:09:41 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,12 +6,6 @@ module.exports = {
       host: "127.0.0.1",
       port: "8545",
       network_id: "*" // match any network id
-    },
-    rinkeby: {
-      host: "localhost",
-      port: 7545,
-      network_id: 4,
-      gas: 4700000
     }
   }
 };
